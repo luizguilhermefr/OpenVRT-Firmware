7d2e9532d43c5dc784c159cde4ebde97e1b30bc1
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-09-04 21:32:12 -0300

    make messsage from response

--- main.ino
+++ main.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include "openvrt.h"
 
 #define BAUD_RATE 9600
 
@@ -8,16 +9,36 @@
 
 #define DELAY 500
 
-SoftwareSerial btSerial(RXPIN, TXPIN);
+SoftwareSerial BTSerial(RXPIN, TXPIN);
+
+bool response_available() {
+    return BTSerial.available() >= MESSAGE_LEN;
+}
+
+openvrt_message_t *make_message_from_command() {
+    char msg_buf[MESSAGE_LEN];
+    for (int i = 0; i < MESSAGE_LEN; i++) {
+        msg_buf[i] = BTSerial.read();
+    }
+    openvrt_message_t *msg = (openvrt_message_t *) msg_buf;
+
+    return msg;
+}
 
 void setup() {
     pinMode(HC05PIN, OUTPUT);
     Serial.begin(BAUD_RATE);
-    // Bluetooth
-    btSerial.begin(BAUD_RATE);
-    connection = new BTConnection(btSerial);
+    BTSerial.begin(BAUD_RATE);
 }
 
 void loop() {
+    if (response_available()) {
+        openvrt_message_t *msg = make_message_from_command();
+        if (is_valid_message(msg)) {
+            Serial.println("Valid.");
+        } else {
+            Serial.println("Invalid.");
+        }
+    }
     delay(DELAY);
 }
--- openvrt.h
+++ openvrt.h
@@ -5,12 +5,30 @@
 #define SIGNATURE_LEN 7
 #define DATA_LEN 8
 
-typedef struct {
+#define SIGNATURE "OPENVRT"
+#define MAJOR_V = 0x0001
+#define MINOR_V = 0x0000
+
+typedef struct message {
     char signature[SIGNATURE_LEN];
     unsigned short major_ver;
     unsigned short minor_ver;
     unsigned int id;
     char opcode;
     char data[DATA_LEN];
-} message;
+} __attribute__((packed)) openvrt_message_t;
+
+bool is_valid_signature(char signature[SIGNATURE_LEN]) {
+    static const char *valid_sign = SIGNATURE;
+    return strcmp(valid_sign, signature) == 0x00;
+}
+
+bool is_valid_message(openvrt_message_t *msg) {
+    if (!is_valid_signature(msg->signature)) {
+        return false;
+    }
+
+    return true;
+}
+
 #endif //OPENVRT_FIRMWARE_OPENVRT_H
