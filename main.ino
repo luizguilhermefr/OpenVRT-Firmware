3143aaf6cb15c9d6148387504781d7fa6e2f16bb
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-09-14 04:05:38 -0300

    parse numeric

--- main.ino
+++ main.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include "openvrt.h"
+#include "treadmill.h"
 
 #define BAUD_RATE 9600
 
@@ -10,6 +11,10 @@
 #define DELAY 100
 
 SoftwareSerial BTSerial(RXPIN, TXPIN);
+
+double current_rate;
+
+char current_measurement;
 
 bool available()
 {
@@ -56,11 +61,44 @@ void print(openvrt_message_t *msg)
   free(message_as_str);
 }
 
+bool next_rate(char data[DATA_LEN])
+{
+  char form[DATA_LEN + 2];
+  for (int i = 0; i < DATA_LEN - 2; i++) { form[i] = data[i]; }
+  form[DATA_LEN - 2] = '.';
+  for (int i = DATA_LEN - 2; i < DATA_LEN; i++) { form[i + 1] = data[i]; }
+  form[DATA_LEN + 2 - 1] = '\0';
+  current_rate = strtod((char *) form, NULL);
+}
+
+bool next_measurement(char data[DATA_LEN])
+{
+  current_measurement = MEASUREMENT_K_HA;
+
+  return true;
+}
+
+bool take_action(openvrt_message_t *msg)
+{
+  switch (msg->opcode) {
+    case RATE_SET:
+      return next_rate(msg->data);
+    case MEASURE_SET:
+      return next_measurement(msg->data);
+    case HANDSHAKE:
+    case ACK_OP:
+    case REFUSE_OP:
+      return true;
+  }
+}
+
 void setup()
 {
   pinMode(HC05PIN, OUTPUT);
   Serial.begin(BAUD_RATE);
   BTSerial.begin(BAUD_RATE);
+  current_rate = 0.0;
+  current_measurement = MEASUREMENT_K_HA;
 }
 
 void loop()
@@ -68,8 +106,18 @@ void loop()
   if (available()) {
     openvrt_message_t *msg = receive();
     if (is_valid_message(msg)) {
-      acknowledge(msg);
-      print(msg);
+      switch (msg->opcode) {
+        case RATE_SET:
+          next_rate(msg->data);
+          Serial.println(current_rate);
+          acknowledge(msg);
+          break;
+        case MEASURE_SET:
+          next_measurement(msg->data) ? acknowledge(msg) : refuse(msg);
+          break;
+        case HANDSHAKE:
+          acknowledge(msg);
+      }
     } else {
       refuse(msg);
     }
